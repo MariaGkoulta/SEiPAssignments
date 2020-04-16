eb495854a602735dd65e1979e4db204aeddfade3
MariaGkoulta <marygkoulta@gmail.com>
2020-04-16 23:03:01 +0300

    Add FileIO class and respective test cases
    With this commit:
    - Added 'FileIO' class which contains a method that reads a file
      and returns an array of the integer numbers it contains
    - Added test cases for the above method and specifically:
    	- Test cases for empty or inexistent file
    	- Test case for expected result of a valid file
    - Added suitable test files in 'test/resources'

---
+++ unittesting/src/test/java/io/FileIOTest.java
@@ -0,0 +1,39 @@
+package io;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class FileIOTest {
+
+	FileIO fileio = new FileIO();
+	public static String resourcesPath = "src/test/resources/";
+
+	@Test
+	public void testReadFileValidInput() {
+		int[] expectedNumbers = new int[] {20, 15, 12, 11, 13, 7, 9, 10};
+		String validInputFilepath = resourcesPath.concat("numbers");
+
+		Assert.assertArrayEquals(expectedNumbers, fileio.readFile(validInputFilepath));
+	}
+
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testNonexistentFile() {
+		String invalidInputFilepath = resourcesPath.concat("xyz");
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Input file does not exist");
+		fileio.readFile(invalidInputFilepath);
+	}
+
+	@Test
+	public void testBothNegativesMultiplication() {
+		String emptyFilepath = resourcesPath.concat("emptyFile");
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Given file is empty");
+		fileio.readFile(emptyFilepath);
+	}
+}
