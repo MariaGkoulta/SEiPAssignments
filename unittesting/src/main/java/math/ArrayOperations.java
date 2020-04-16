bad35639f89a80c85399f93156e4afa87998e701
MariaGkoulta <marygkoulta@gmail.com>
2020-04-16 23:41:24 +0300

    Add ArrayOperations class and respective test cases
    With this commit:
    - Added a method in class 'ArrayOperations' which
      iterates in an array of numbers and returns a new
      array that contains only prime numbers
    - Added test cases for the above method by using the Mockito
      features to mock the FileΙΟ and MyMath instances that are passed
      as parameters in the method.

---
+++ unittesting/src/main/java/math/ArrayOperations.java
@@ -0,0 +1,46 @@
+package math;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import io.FileIO;
+
+/**
+ * This class provides method to find
+ * prime number in a file
+ * 
+ * @author Maria Gkoulta
+ *
+ */
+public class ArrayOperations {
+	
+	/**
+	 * Performs the operation of factorial
+	 * @param number for which the factorial is calculated
+	 * @return the result of the factorial
+	 * @exception IllegalArgumentException when the number 
+	 * is less than zero or greater than twelve  
+	 */
+	public int[] findPrimesInFile(FileIO fileIo, String filepath, MyMath myMath) {
+		int[] numbers = fileIo.readFile(filepath);
+		List<Integer> primes = new ArrayList<>();
+		for (int i = 0; i < numbers.length; i++) {
+			if (myMath.isPrime(numbers[i])) {
+			  primes.add(numbers[i]);
+			}
+		}
+		return primes.stream().mapToInt(i -> i).toArray();
+	}
+
+
+	public static void main(String[] args) {
+		ArrayOperations obj = new ArrayOperations();
+		FileIO fileIo = new FileIO();
+		MyMath myMath = new MyMath();
+		int[] arr = obj.findPrimesInFile(fileIo, "src/test/resources/numbers", myMath);
+		for (int i = 0; i < arr.length; i++) {
+		System.out.println(arr[i]);
+		}
+	}
+
+}
