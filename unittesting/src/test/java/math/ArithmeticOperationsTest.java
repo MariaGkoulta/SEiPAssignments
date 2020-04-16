5bf13fd06e4ee063378f95c6160f3462bb247041
MariaGkoulta <marygkoulta@gmail.com>
2020-04-16 22:28:20 +0300

    Add tests for Arithmetic operations
    With this commit:
    - Removed unintentionally added 'MyMath' class
    - Added class 'ArithmeticOperations'
    - Added tests for class 'ArithmeticOperations'
    
    The test cases check:
    - Normal division and multiplication
    - Division with zero
    - Multiplication of negatives and opposite numbers
    - Multiplication which doesn't fit in integer variable

---
+++ unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -0,0 +1,60 @@
+package math;
+
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Assert;
+import org.junit.Rule;
+
+public class ArithmeticOperationsTest {
+	
+	ArithmeticOperations obj = new ArithmeticOperations();
+
+	@Test
+	public void testDivide() {
+		Assert.assertEquals(5.0, obj.divide(20.0, 4.0), 0.00001);
+	}
+	
+	@Test(expected=ArithmeticException.class)
+	public void testDevideByZero() {
+		obj.divide(2,0);
+	}
+	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
+	@Test
+	public void testBothNegativesMultiplication() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("x & y should be >= 0");
+		obj.multiply(-1, -4);
+	} 
+	
+	@Test
+	public void testIntegerFit() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("The product does not fit in an Integer variable");
+		obj.multiply(2, Integer.MAX_VALUE);
+	} 
+	
+	@Test
+	public void testNegativeXMultiplication() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("x & y should be >= 0");
+		obj.multiply(-1, 5);
+	} 
+	
+	@Test
+	public void testNegativeYMultiplication() {
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("x & y should be >= 0");
+		obj.multiply(1, -5);
+	}
+	
+	@Test
+	public void testMutiplication() {
+		Assert.assertEquals(20, obj.multiply(4, 5));
+	}
+
+}
+
+	
